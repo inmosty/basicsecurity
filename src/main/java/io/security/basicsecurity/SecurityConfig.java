4a6bcd7cc217733f54a97bc3940061ce397e9b85
jkpark <inmosty@gmail.com>
2023-12-23 22:13:41 +0900

    커밋

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -3,12 +3,22 @@ package io.security.basicsecurity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,11 +29,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         //인가 정책
-        http
+/*        http
                 .authorizeHttpRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated();*/
 
         //인증 정책
         http
@@ -36,7 +53,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login_proc")
                 .successHandler((request, response, authentication) -> {
                     log.info("authentication : {}", authentication.getName());
-                    response.sendRedirect("/");
+                    RequestCache requestCache = new HttpSessionRequestCache();
+                    SavedRequest savedRequest = requestCache.getRequest(request, response);
+                    String redirectUrl = savedRequest.getRedirectUrl();
+                    log.info("redirectUrl : {}", redirectUrl);
+                    response.sendRedirect(redirectUrl);
                 })
                 .failureHandler((request, response, exception) -> {
                     log.info("exception : {}", exception.getMessage());
@@ -62,5 +83,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .tokenValiditySeconds(3600)
                 .userDetailsService(userDetailsService);
 
+        //동시세션제어
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true) //true 이면 나중에 접속 시도한 사람이 실패함 false 이면 기존사용자가 실패함
+                ;
+
+        // 세션 고정 보호
+        http
+                .sessionManagement()
+                .sessionFixation().changeSessionId(); //로그인시에 세션값을 변경한다
+
+
+        http
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .anyRequest().authenticated();
+
+/*        http
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    log.info("commence authenticationEntryPoint");
+                    response.sendRedirect("/login");
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    log.info("handle accessDeniedException");
+                    response.sendRedirect("/denied");
+                });*/
+
+        //csrf 기능
+        http
+                .csrf();  //기본적으로 켜져있다.
     }
+
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,20 +1,63 @@
 package io.security.basicsecurity;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        log.info("authentication = {}", authentication);
+        log.info("context = {}", context);
         return "home";
     }
 
     @GetMapping("loginPage")
     public String loginPage() {
         return "loginPage";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+    @GetMapping("/admin/**")
+    public String admin() {
+        return "admin";
+    }
+    @GetMapping("/denied")
+    public String denied() {
+        return "denied";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+        new Thread(
+                () -> {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    log.info("authentication = {}", authentication);
+                }
+        );
+        return "thread";
     }
 }
